ba7ddeb084029e33c40ce4e2e15d8e9bc380e089
Alex <alex@lite-check.com>
2023-01-04 14:39:12 -0800

    Init Commit

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+int x;
+int y;
+
+x = 7;
+y = x + 3;
+
+Console.WriteLine(y);
+*/
+
+/*
+string myFirstName;
+myFirstName = "Alex";
+*/
+
+string myFirstName = "Alex";
+
+Console.WriteLine(myFirstName);
+
+int x = 7;
+string y = "Mikey";
+string myFirstTry = x + y;
+
+
+Console.WriteLine(myFirstTry);
+
+Console.ReadLine();
