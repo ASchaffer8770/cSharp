b29a89d6f27e892bfa67c8ae2e4d57b84f87309d
Alex <alex@lite-check.com>
2023-01-05 13:04:42 -0800

    Proxy Class

--- UWP Weather/UWP Weather/MainPage.xaml.cs
+++ UWP Weather/UWP Weather/MainPage.xaml.cs
@@ -26,5 +26,10 @@ namespace UWP_Weather
         {
             this.InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
