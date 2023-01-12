ebe2288eedf3d93599c266c6cda8a97037cde7c8
Alex <alex@lite-check.com>
2023-01-12 14:27:06 -0800

    more basic learning for C#

--- Variables/Variables/Program.cs
+++ Variables/Variables/Program.cs
@@ -1,27 +1,16 @@
-﻿/*
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
 int x;
 int y;
 
 x = 7;
 y = x + 3;
-
 Console.WriteLine(y);
 */
 
-/*
 string myFirstName;
 myFirstName = "Alex";
-*/
-
-string myFirstName = "Alex";
-
 Console.WriteLine(myFirstName);
-
-int x = 7;
-string y = "Mikey";
-string myFirstTry = x + y;
-
-
-Console.WriteLine(myFirstTry);
 
 Console.ReadLine();
