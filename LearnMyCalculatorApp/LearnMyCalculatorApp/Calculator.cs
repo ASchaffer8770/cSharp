8930894a4379fdf8247ed0e5ffbb75d5cd43ea18
Alex <alex@lite-check.com>
2023-01-20 10:10:17 -0800

    init commit

---
+++ LearnMyCalculatorApp/LearnMyCalculatorApp/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Calculator
+{
+    public Calculator
+	{
+		public int Add(int x, int y)
+    {
+        return x + y;
+    }
+    public int Subtract(int x, int y)
+    {
+        return x - y;
+    }
+    public Multiply(int x, int y)
+    {
+        return x * y;
+    }
+    public Divide(int x, int y)
+    {
+        return x / y;
+    }
+}
+}
