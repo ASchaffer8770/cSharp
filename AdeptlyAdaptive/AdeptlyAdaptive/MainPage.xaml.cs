db07b9852f57a23521181866ef652eaf58903316
Alex <alex@lite-check.com>
2023-01-25 11:00:31 -0800

    class construction and data binding

--- AdeptlyAdaptive/AdeptlyAdaptive/MainPage.xaml.cs
+++ AdeptlyAdaptive/AdeptlyAdaptive/MainPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using AdeptlyAdaptive.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -22,9 +24,36 @@ namespace AdeptlyAdaptive
     /// </summary>
     public sealed partial class MainPage : Page
     {
+
+        private ObservableCollection<NewsItem> NewsItems;
         public MainPage()
         {
             this.InitializeComponent();
+            NewsItems = new ObservableCollection<NewsItem>();
+        }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (Financial.IsSelected)
+            {
+                NewsManager.GetNews("Financial", NewsItems);
+                TitleTextBlock.Text = "Financial";
+            }
+            else if (Food.IsSelected)
+            {
+                NewsManager.GetNews("Food", NewsItems);
+                TitleTextBlock.Text = "Food";
+            }
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            Financial.IsSelected = true;
         }
     }
 }
